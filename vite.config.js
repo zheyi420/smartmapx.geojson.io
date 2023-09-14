517c2111478fcd3eecf2b5aae182fa1858d362d7
zheyi420 <37471153+zheyi420@users.noreply.github.com>
2023-09-14 23:13:22 +0800

    config for gh-pages ...

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? '/smartmapx.geojson.io/' : '/', // https://vitejs.dev/guide/static-deploy.html#github-pages
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
